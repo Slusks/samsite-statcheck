21e40cb7814f3c94fdf8ce9a229c3d887798cddb
sam <samslusky@gmail.com>
2021-11-20 11:08:01 -0500

    Updated cover photo

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
+  images: string[] =["../assets/img/damwon.jpg",
+  "../assets/img/FPX.jpg",
+  "../assets/img/2015skt.webp",
+  "../assets/img/2017SSG.webp",
+  "../assets/img/2014SSW.webp",
+  "../assets/img/TPA.webp",
+  "../assets/img/Fnatic.webp"]
 
+  backgroundImage: string = '';
   constructor() { }
 
   ngOnInit() {
+    let ran = Math.floor(Math.random()*6);
+    console.log(ran, Math.random()*100)
+    this.backgroundImage = this.images[ran];
   }
 
 }
